9a0c05d84b816ac2ce7bac1555161e5b30974ac4
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-06 10:37:04 +0200

    added every method, for all test.

--- 01_Calculator/Calculator.cs
+++ 01_Calculator/Calculator.cs
@@ -1,15 +1,55 @@
-﻿namespace _01_Calculator
+﻿using System;
+
+namespace _01_Calculator
 {
-    public class Calculator
+    internal class Calculator
     {
-        public static int Add(int no1, int no2)
+        internal static int Add(int no1, int no2)
         {
             return no1 + no2;
         }
 
-        public static object Subtract(int no1, int no2)
+        internal static object Subtract(int no1, int no2)
         {
             return no1 - no2;
+        }
+
+        internal static int Sum(int[] numbers)
+        {
+            int _sum = 0;
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                _sum += numbers[i];
+            }
+            return _sum;
+        }
+
+        internal static int Multiply(int[] numbers)
+        {
+            int _multiplySum = 1;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                _multiplySum *= numbers[i];
+            }
+
+            return _multiplySum;
+        }
+
+        internal static double Power(double no1, double no2)
+        {
+            return Math.Pow(no1, no2);
+        }
+
+        internal static int Factorial(int no1)
+        {
+            int result = 1;
+            for (int i = no1; i >= 1; i--)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
--- 01_Calculator/CalculatorTest.cs
+++ 01_Calculator/CalculatorTest.cs
@@ -25,37 +25,49 @@ namespace _01_Calculator
             int[] numbers = new int[] { };
             Assert.AreEqual(0, Calculator.Sum(numbers));
 
+            numbers = new int[1];
             numbers[0] = 7;
             Assert.AreEqual(7, Calculator.Sum(numbers));
 
+            numbers = new int[2];
             numbers[0] = 7;
             numbers[1] = 11;
             Assert.AreEqual(18, Calculator.Sum(numbers));
+
+            numbers = new int[5] { 7, 11, 0, 0, 10 };
+            Assert.AreEqual(28, Calculator.Sum(numbers));
 
         }
         [TestMethod]
         public void Multiply()
         {
-            //It multiplies two numbers
-            //It multiplies several numbers
-            throw new NotImplementedException();
+            //Arrange
+            int[] numbers = new int[] { };
+            Assert.AreEqual(1, Calculator.Multiply(numbers));
+
+            numbers = new int[2];
+            numbers[0] = 2;
+            numbers[1] = 3;
+            Assert.AreEqual(6, Calculator.Multiply(numbers)); //It multiplies two numbers
+
+            numbers = new int[5] { 2, 3, 5, 3, 2 };
+            Assert.AreEqual(180, Calculator.Multiply(numbers)); //It multiplies several numbers
         }
         [TestMethod]
         public void Power()
         {
-            //It raises one number to the power of another number
-            throw new NotImplementedException();
+            Assert.AreEqual(4, Calculator.Power(2, 2)); //It raises one number to the power of another number
         }
         [TestMethod]
         public void Factorial()
         {
+            Assert.AreEqual(1, Calculator.Factorial(0)); //  it "computes the factorial of 0"
+            Assert.AreEqual(1, Calculator.Factorial(1)); //  it "computes the factorial of 1"
+            Assert.AreEqual(2, Calculator.Factorial(2)); //  it "computes the factorial of 2"
+            Assert.AreEqual(120, Calculator.Factorial(5)); //  it "computes the factorial of 5"
+            Assert.AreEqual(3628800, Calculator.Factorial(10)); //  it "computes the factorial of 10"
+
             //# http://en.wikipedia.org/wiki/Factorial
-            //  it "computes the factorial of 0"
-            //  it "computes the factorial of 1"
-            //  it "computes the factorial of 2"
-            //  it "computes the factorial of 5"
-            //  it "computes the factorial of 10"
-            throw new NotImplementedException();
         }
     }
 }
