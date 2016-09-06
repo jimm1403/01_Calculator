88390ca3ab163a24a1fcc0580854cf19288dd7e2
jimmi <jimmi@DESKTOP-2OTI0T5>
2016-09-06 08:19:26 +0200

    added calculator class, and "add" and "subtract" methods.

---
+++ 01_Calculator/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_Calculator
+{
+    public class Calculator
+    {
+        public static int Add(int no1, int no2)
+        {
+            return no1 + no2;
+        }
+
+        public static object Subtract(int no1, int no2)
+        {
+            return no1 - no2;
+        }
+    }
+}
